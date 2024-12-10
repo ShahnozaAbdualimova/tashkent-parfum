3e749374a5fe25994858ef9c79fa3fd5fabc038d
Shahnoza <shahnozaabdualimova191@gmail.com>
2024-12-10 22:17:51 +0500

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    '~/assets/css/main.css'
+  ],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss']
+})
