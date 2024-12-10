1b1ea2ab2abd1a1c7a67ac5a2bc3e52a4ccd27f8
Shahnoza <shahnozaabdualimova191@gmail.com>
2024-12-10 22:19:41 +0500

    Setup Nuxt.js with TailwindCSS and custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./pages/**/*.{js,ts,vue}", "./components/**/*.{js,ts,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        proxima: ["Proxima", "sans-serif"], 
+        proxima: ["Proxima", "sans-serif"],
       },
     },
   },
