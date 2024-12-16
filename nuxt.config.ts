7f6b95369f15098c7c223b8da9b501d84be2c25d
Shakhrizada <example@gmail.com>
2024-12-16 11:46:43 +0500

    fix: add input component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,5 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-swiper']
+  modules: ['@nuxtjs/tailwindcss']
 })
