e0d033f1e9fccc01d3122316d07cf2f75fe2cfa5
jaxangir <example@gmail.com>
2024-12-21 17:53:06 +0500

    fix: counter button added to card

--- layouts/LHeader.vue
+++ layouts/LHeader.vue
@@ -1,51 +1,32 @@
 <template>
-    <div>
-      <Input
-        v-model="username"
-        placeholder="Enter your username"
-        :error="hasError"
-        inputClass="bg-white"
-      >
-        <template #prefix>
-          <span class="mr-2">👤</span>
-        </template>
-        <template #suffix>
-          <span v-if="hasError" class="text-red-500">!</span>
-        </template>
-      </Input>
-
-      hi
-    </div>
-  </template>
-  
-  <script setup>
-  import Input from '@/components/Input.vue';
-  import { ref } from 'vue';
-  
-  const username = ref('');
-  const hasError = ref(false);
-  </script>
-  <header class="p-4">
-    <h1 class="text-xl font-semibold">Select Language</h1>
-    <Dropdown
-      :selectedValue="selectedLanguage"
-      :options="languages"
-      @update:selectedValue="setSelectedLanguage"
-    />
-  </header>
+  <div>
+    <Input
+      v-model="username"
+      placeholder="Enter your username"
+      :error="hasError"
+      inputClass="bg-white"
+    >
+      <template #prefix>
+        <span class="mr-2">👤</span>
+      </template>
+      <template #suffix>
+        <span v-if="hasError" class="text-red-500">!</span>
+      </template>
+    </Input>
+  </div>
 </template>
 
 <script>
-import Dropdown from '~/components/base/dropdown.vue'; 
+import Dropdown from '~/components/base/dropdown.vue';
 
 export default {
   components: {
-    Dropdown, 
+    Dropdown,
   },
   data() {
     return {
-      selectedLanguage: 'O‘zbekcha', 
-      languages: ['O‘zbekcha', 'Русский', 'English'], 
+      selectedLanguage: 'O‘zbekcha',
+      languages: ['O‘zbekcha', 'Русский', 'English'],
     };
   },
   methods: {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
-
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
-})
-
   modules: ['@nuxtjs/tailwindcss'],
   app: {
     head: {
@@ -14,9 +10,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '_nuxt/public/favicon.svg' },
-      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
     },
   },
 });
