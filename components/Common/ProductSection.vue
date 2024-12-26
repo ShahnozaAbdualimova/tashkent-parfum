fbe426de41c99b14fd00f7c67a3e29a6f9e8f47e
sanjarbarakayev <genius.s.1799@gmail.com>
2024-12-26 18:33:57 +0500

    chore: add components

---
+++ components/Common/ProductSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto mt-6">
+    <CommonSectionWrapper title="Рекомендуемые товары" linkText="Все товары">
+      <div class="grid grid-cols-6 gap-3">
+        <BaseProductCard v-for="(product, index) in products.slice(0, 6)" :key="index" :product="product" />
+      </div>
+    </CommonSectionWrapper>
+  </div>
+</template>
+  
+  <script setup>
+  import { products } from '~/data/mainProductSection';
+  </script>
+  
