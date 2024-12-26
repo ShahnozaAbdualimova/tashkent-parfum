d971705e298feaaa3ac80b7b9ed47ebd10bc47b7
sanjarbarakayev <genius.s.1799@gmail.com>
2024-12-26 20:32:27 +0500

    fix: btn

--- components/Base/Button.vue
+++ components/Base/Button.vue
@@ -50,15 +50,6 @@ const computedClass = computed(() => {
       'transform transition-transform active:scale-95 bg-red-950  px-4 py-2.5 font-normal  text-red-500',
     light:
       ' px-4 py-3 bg-transparent rounded-lg font-normal text-base text-white  hover:bg-white hover:text-black-300 group border border-white',
-  };
-    headeBtn: "",
-    primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "font-proxima rounded-md transform transition-transform active:scale-95 bg-white-400 duration-300 px-4 py-2.5 font-normal text-sm text-black",
-    secondaryGray: "bg-white-500 text-white font-proxima rounded-md duration-300 px-4 py-2.5 font-normal text-sm",
-    lightDanger: "font-proxima rounded-md transform transition-transform active:scale-95 bg-red-950 duration-300 px-4 py-2.5 font-normal text-sm text-red-500",
-    danger:
-      "font-proxima rounded-md transform transition-transform active:scale-95 bg-red-500 hover:shadow-[0_0_10px_1px_rgba(246,37,89,0.5)] duration-300 px-7 py-2.5 font-normal text-sm text-white",
-      light: "font-proxima px-4 py-3 bg-transparent rounded-lg font-normal text-base text-white duration-300 hover:bg-white hover:text-black-300 group border border-white"
   }[props.variant];
 
   let iconPositionClass;
@@ -69,7 +60,7 @@ const computedClass = computed(() => {
 
   const btnVariantClass = variantClass[props.variant];
 
-  return [baseClass, btnVariantClass, iconPosition];
+  return [baseClass, btnVariantClass, iconPositionClass];
 });
 
 const emit = defineEmits(['click']);
--- components/Common/ProductSection.vue
+++ components/Common/ProductSection.vue
@@ -1,27 +1,19 @@
 <template>
-  <section class="w-full mt-16 mb-11">
-    <div class="container mx-auto">
-      <CommonSectionWrapper
-      title="Рекомендуемые товары"
-      link-text="Все товары"
-      class=""
-    >
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3"
-      >
-        <commonProductCard
+  <div class="container mx-auto mt-6">
+    <CommonSectionWrapper title="Рекомендуемые товары" linkText="Все товары">
+      <div class="grid grid-cols-6 gap-3">
+        <BaseProductCard
           v-for="(product, index) in products.slice(0, 6)"
           :key="index"
           :product="product"
         />
+
+
       </div>
     </CommonSectionWrapper>
-    </div>
-  </section>
+  </div>
 </template>
 
 <script setup>
-// import { products } from '~/data/mainProductSection';
 import { products } from '~/data/mainProductSection';
 </script>
-  
