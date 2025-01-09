e3bdd935ee3d951565972840221fa675eae403dd
milliydev <zohidtojiyev071@gmail.com>
2025-01-09 13:29:41 +0500

    footer

--- components/Common/ProductReviewSlider.vue
+++ components/Common/ProductReviewSlider.vue
@@ -1,36 +1,37 @@
 <template>
-    <CommonCSectionWrapper title="Отзывы" />
-    <div class="h-screen">
-        <Swiper class="h-full " :slides-per-view="3.5" :space-between="30" :breakpoints="{
-            0: { slidesPerView: 1, spaceBetween: 10 },
-            640: { slidesPerView: 1.5, spaceBetween: 20 },
-            768: { slidesPerView: 2.5, spaceBetween: 25 },
-            1000: { slidesPerView: 3.5, spaceBetween: 30 }
-        }" pagination>
-            <SwiperSlide v-for="(testimonial, index) in testimonials" :key="index">
-                <div class="bg-white rounded-lg p-4 shadow-sm text-left max-h-52 custom-shadow cursor-pointer">
-                    <img :src="testimonial.image" alt="Product Image" class="w-12 h-12 object-cover -mb-12" />
-                    <h3 class="text-lg font-bold pl-20">{{ testimonial.title }}</h3>
-                    <p class="text-sm text-gray-600 max-w-64 ml-20">
-                        {{ testimonial.description }}
-                    </p>
-                    <div class="flex items-center gap-3 mt-3">
-                        <img :src="testimonial.reviewerImage" alt="Reviewer Image"
-                            class="w-8 h-8 rounded-full object-cover" />
-                        <div>
-                            <p class="text-sm font-semibold">{{ testimonial.reviewerName }}</p>
-                            <p class="text-xs text-gray-500">
-                                {{ testimonial.date }} - {{ testimonial.time }}
-                            </p>
+    <CommonCSectionWrapper title="Отзывы">
+        <div class="h-screen">
+            <Swiper class="h-full w-screen" :slides-per-view="3.5" :space-between="30" :breakpoints="{
+                0: { slidesPerView: 1, spaceBetween: 10 },
+                640: { slidesPerView: 1.5, spaceBetween: 20 },
+                768: { slidesPerView: 2.5, spaceBetween: 25 },
+                1000: { slidesPerView: 3.5, spaceBetween: 30 }
+            }" pagination>
+                <SwiperSlide v-for="(testimonial, index) in testimonials" :key="index">
+                    <div class="bg-white rounded-lg p-4 shadow-sm text-left max-h-52 custom-shadow cursor-pointer">
+                        <img :src="testimonial.image" alt="Product Image" class="w-12 h-12 object-cover -mb-12" />
+                        <h3 class="text-lg font-bold pl-20">{{ testimonial.title }}</h3>
+                        <p class="text-sm text-gray-600 max-w-64 ml-20">
+                            {{ testimonial.description }}
+                        </p>
+                        <div class="flex items-center gap-3 mt-3">
+                            <img :src="testimonial.reviewerImage" alt="Reviewer Image"
+                                class="w-8 h-8 rounded-full object-cover" />
+                            <div>
+                                <p class="text-sm font-semibold">{{ testimonial.reviewerName }}</p>
+                                <p class="text-xs text-gray-500">
+                                    {{ testimonial.date }} - {{ testimonial.time }}
+                                </p>
+                            </div>
                         </div>
+                        <p class="text-sm font-bold text-yellow-400 mt-3">
+                            ⭐ ⭐ ⭐ ⭐ {{ testimonial.rating }}
+                        </p>
                     </div>
-                    <p class="text-sm font-bold text-yellow-400 mt-3">
-                        ⭐ ⭐ ⭐ ⭐ {{ testimonial.rating }}
-                    </p>
-                </div>
-            </SwiperSlide>
-        </Swiper>
-    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    </CommonCSectionWrapper>
 </template>
 
 <script>
--- components/Layout/Navbar.vue
+++ components/Layout/Navbar.vue
@@ -2,25 +2,17 @@
   <div class="w-full border-t border-white-400 bg-white">
     <div class="container">
       <div class="flex items-center justify-between h-16 px-8">
-        <div
-          v-for="(link, index) in links"
-          :key="link.title"
-          class="relative group overflow-hidden cursor-pointer flex items-center whitespace-nowrap mx-1 sm:mx-2"
-        >
+        <div v-for="(link, index) in links" :key="link.title"
+          class="relative group overflow-hidden cursor-pointer flex items-center whitespace-nowrap mx-1 sm:mx-2">
           <span
-            class="block text-sm font-medium text-gray-800 group-hover:translate-y-[-100%] group-hover:text-red-500 transition-transform duration-200"
-          >
+            class="block text-sm font-medium text-gray-800 group-hover:translate-y-[-100%] group-hover:text-red-500 transition-transform duration-200">
             {{ link.title }}
           </span>
           <span
-            class="absolute inset-0 text-sm font-medium text-red-500 translate-y-[100%] group-hover:translate-y-0 transition-transform duration-200"
-          >
+            class="absolute inset-0 text-sm font-medium text-red-500 translate-y-[100%] group-hover:translate-y-0 transition-transform duration-200">
             {{ link.title }}
           </span>
-          <div
-            v-if="index !== links.length - 1"
-            class="hidden sm:block w-px h-4 bg-gray-300 ml-6"
-          ></div>
+          <div v-if="index !== links.length - 1" class="hidden sm:block w-px h-4 bg-gray-300 ml-6"></div>
         </div>
       </div>
     </div>
