b62190f1c02b2ae7b3dade274f8bef709037969a
milliydev <zohidtojiyev071@gmail.com>
2025-01-16 18:06:08 +0500

    footer responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <LayoutSubHeader class="hidden lg:block" />
-    <LayoutHeader class="mb-7" />
+    <!-- <LayoutHeader class="mb-7" /> -->
     <NuxtPage />
-    <!-- <LayoutFooter /> -->
+    <LayoutFooter />
   </div>
 </template>
