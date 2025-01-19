574c2b26610e8b35573ab492bbd042420a63a92b
jaxangir <example@gmail.com>
2025-01-20 04:47:43 +0500

    style:Fix padding issues

--- pages/playground.vue
+++ pages/playground.vue
@@ -1,23 +1,7 @@
 <template>
   <div>
-    <!-- <CommonStepper
-      :steps="[
-        {
-          id: 1,
-          label: 'Адрес доставки',
-          icon: 'icon-location',
-        },
-        {
-          id: 2,
-          label: 'Контактные данные',
-          icon: 'icon-contact',
-        },
-        {
-          id: 3,
-          label: 'Оплата',
-          icon: ' icon-wallet',
-        },
-      ]"
-    /> -->
+    <CommonTopBrandsSection />
   </div>
 </template>
+
+<script setup lang="ts"></script>
